02c7112717f6c7bacba648a7c7fb76734225f1b2
gamy7 <gamy710@gmail.com>
2022-04-12 16:18:36 -0500

    12. se creo y realizo el ejemplo9

---
+++ weekly_mission_1/example9/main.js
@@ -0,0 +1,19 @@
+*/Manda a llamar el archivo  llamado pokemon,  todo ese archivo  ponlo en esta variable */
+const Pokemon = require('./pokemon')
+
+const pikachu = new Pokemon("pikachu")
+const bulbasaur = new Pokemon("bulbasaur")
+const squirtle = new Pokemon("squirtle")
+const charmander = new Pokemon("charmander")
+
+pikachu.sayHello()
+pikachu.sayMessage("Heey!")
+
+bulbasaur.sayHello()
+bulbasaur.sayMessage("Heey!")
+
+charmander.sayHello()
+charmander.sayMessage("Heey!")
+
+squirtle.sayHello()
+squirtle.sayMessage("Heey!")
---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,9 @@
+/*
+se crearan las funciones a exportar
+*/
+module.exports.verbose = (message) => {
+  console.log(`verbose: ${message}`)
+}
+
+
+
